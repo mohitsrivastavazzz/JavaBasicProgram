93722e93fa035e5e1e759ef92a7eeb2a9425f74e
Mohit <Mohit@LAPTOP-FSNUKQ7G.mshome.net>
2022-04-06 15:17:32 +0530

    fifth commit

---
+++ JavaBasicProgram/src/Unit_01/P5_Task01_ClassAndObjectInJava.java
@@ -0,0 +1,24 @@
+package Unit_01;
+
+public class P5_Task01_ClassAndObjectInJava {
+	
+	public static void main(String[] args) {
+		ABC2 obj = new ABC2();
+		obj.display2();
+		ABC2.display();
+		}
+}
+		class ABC2 {
+		int a = 10;
+		static int b = 16;
+		static void display() {
+		int b = 10;
+		b++;
+		System.out.println(b);
+	
+		}
+		
+		void display2() {
+			System.out.println(a);
+		}
+}
